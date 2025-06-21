91832e88b7acf428936a9b7343a3d88c1a8f3286
mudassir <mudassirkhantailor@gmail.com>
2025-06-21 20:45:59 +0530

    Added "Dashboard Home Screen"

---
+++ app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { IconArrowRight } from "@tabler/icons-react";
+import Image from "next/image";
+import React from "react";
+
+type doctorAgent = {
+  id: number;
+  specialist: string;
+  description: string;
+  image: string;
+  agentPrompt: string;
+};
+
+type props = {
+  doctorAgent: doctorAgent;
+};
+
+function DoctorAgentCard({ doctorAgent }: props) {
+  console.log("doctor:", doctorAgent);
+
+  return (
+    <div className="">
+      <Image src={doctorAgent.image} alt={doctorAgent.specialist} width={200} height={300} className="w-full h-[250px] object-cover rounded-xl" />
+      <h2 className="font-bold text-lg mt-1">{doctorAgent.specialist}</h2>
+      <p className="line-clamp-2  text-sm text-gray-500">{doctorAgent.description}</p>
+      <Button className="w-full mt-2">
+        Start Consultation <IconArrowRight />{" "}
+      </Button>
+    </div>
+  );
+}
+
+export default DoctorAgentCard;
